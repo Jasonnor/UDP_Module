82c03e19b0e280aac68406fa06ae2577e40cfc3c
Jasonnor <wujason810@gmail.com>
2016-12-10 21:37:57 +0800

    Update BroadcastClient

--- src/UDPServer/BroadcastClient.java
+++ src/UDPServer/BroadcastClient.java
@@ -5,22 +5,41 @@ import java.util.ArrayList;
 
 import org.json.*;
 
-public class BroadcastClient {
+public class BroadcastClient extends Thread {
 
-    public void startUDPBroadCast() throws Exception {
+    public void run() {
+        try {
+            startUDPBroadCast();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startUDPBroadCast() throws Exception {
         ArrayList<InetAddress> clientAddresses = TCPServerModule.getClientIPTable();
+        ArrayList<JSONObject> firstEncodeInfo = updateInfo("ADD");
+        Broadcast firstBroadcast = new Broadcast(clientAddresses, firstEncodeInfo);
+        firstBroadcast.start();
+        while (true) {
+            ArrayList<JSONObject> encodeInfo = updateInfo("UPDATE");
+            Broadcast broadcast = new Broadcast(clientAddresses, encodeInfo);
+            broadcast.start();
+            sleep(200);
+        }
+    }
+
+    private ArrayList<JSONObject> updateInfo(String command) {
         ArrayList<JSONObject> updateInfo = CentralizedDataCenter.getUpdateInfo();
         ArrayList<JSONObject> encodeInfo = new ArrayList<>();
         for (JSONObject info : updateInfo) {
             Object character = info.getJSONObject("Character");
             Object item = info.getJSONObject("Item");
             JSONObject newInfo = new JSONObject();
-            newInfo.append("Command", "ADD");
+            newInfo.append("Command", command);
             newInfo.append("Character", character);
             newInfo.append("Item", item);
             encodeInfo.add(newInfo);
         }
-        Broadcast broadcast = new Broadcast(clientAddresses, encodeInfo);
-        broadcast.start();
+        return encodeInfo;
     }
 }
