b0b140a8cf001197e357dda8e725f3da8b27669f
Jasonnor <wujason810@gmail.com>
2016-12-10 21:52:21 +0800

    Update receive function

--- src/UDPClient/UpdateServer.java
+++ src/UDPClient/UpdateServer.java
@@ -29,9 +29,15 @@ public class UpdateServer extends Thread {
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             DatagramSocket socket = new DatagramSocket(port);
             socket.receive(packet);
-            String msg = new String(buffer, 0, packet.getLength());
-            JSONObject newInfo = new JSONObject();
-            System.out.println(msg);
+            String receiveMessage = new String(buffer, 0, packet.getLength());
+            JSONObject message = new JSONObject(receiveMessage);
+            if (message.get("Command").equals("ADD")) {
+                DOM.addVirutalCharacter(message.getJSONObject("Character"));
+                DOM.addItem(message.getJSONObject("Item"));
+            } else if (message.get("Command").equals("UPDATE")) {
+                DOM.updateVirutalCharacter(message.getJSONObject("Character"));
+                DOM.updateItem(message.getJSONObject("Item"));
+            }
             socket.close();
         }
     }
